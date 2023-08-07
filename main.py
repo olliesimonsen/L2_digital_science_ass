bbb5de446ff55b8d96e2ad7b2d7548dbf0dc1d9e
olliesimonsen <simonseno@stu.otc.school.nz>
2023-08-07 15:20:07 +1200

    Commented and changes some positions and sizes of buttons

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-"""An educational game"""
+"""An educational game."""
 import random
 import re
-import pygame
 import sys
+import pygame
 
 # pylint: disable=no-member, W0123
 
-# global variables
-
-
-# main window
+# The main window specifications.
 pygame.init()
 win_height, win_width = 1080, 1920
 res = (win_width, win_height)
@@ -19,11 +16,7 @@ pygame.display.set_caption("Stellar Solver")
 # Constant Variables.
 FPS = 60
 
-# Changing variables.
-LIVES = 3
-
-
-# RGB colour values
+# RGB colour values.
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (183, 83, 75)
@@ -31,23 +24,22 @@ YELLOW = (255, 255, 0)
 BLUE = (93, 135, 194)
 GREEN = (0, 255, 0)
 
-# added events
+# Events added to more fluidly run the program.
 PLAYER_HIT = pygame.USEREVENT + 1
 ENEMY_HIT = pygame.USEREVENT + 2
 
-# defining an image to be used in the game
+# Defining an image to be used in the game.
 control_image = pygame.image.load(
     "Controls_Screen.png").convert()
 
 spaceship = pygame.image.load("spaceship_red.PNG").convert()
+
+# Altering these images to fit the games dimensions.
 spaceship = pygame.transform.scale(spaceship, (60, 60))
 spaceship = pygame.transform.rotate(spaceship, 90)
 
-# makes new button rectangles using the Button class
-
-
 class Button:
-    """makes rectangles for buttons"""
+    """Classes for rectanglualr objects that have a desired function when clicked."""
 
     def __init__(self, x_cord, y_cord, width, height, colour, button_func):
 
@@ -59,36 +51,41 @@ class Button:
         self.func = button_func
 
     def make_button(self):
-        """makes the rectangles for the button class"""
+        """Makes and returns a rectangle for the button class."""
         button = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
         return button
 
 
-# base balck rectangle
+# The Basic Black backround every window is built on.
 baseplate = Button(0, 0, win_width, win_height, BLACK, "")
 
-# all the buttons on the main menu
-play_button = Button(win_width/2 - 60, 100, 120, 50, WHITE, "gameplay_main()")
+# All the button and rectangles on the main menu.
+button_width, button_height = 200, 100
+play_button = Button(
+    win_width/2 - button_width/2, 200, button_width, button_height, WHITE, "gameplay_main()")
+
 highscore_button = Button(
-    win_width/2 - 60, 175,  120, 50, WHITE, "highscore_main()")
-controls_button = Button(win_width/2 - 60, 250, 120,
-                         50, WHITE, "control_main()")
+    win_width/2 - button_width/2, 350,  button_width, button_height, WHITE, "highscore_main()")
+
+help_button = Button(win_width/2 - button_width/2, 500, button_width,
+                         button_height, WHITE, "help_main()")
+
 main_menu_ex_cordit_button = Button(
-    win_width/2 - 60, 325,  120, 50, WHITE, "sys.exit()")
+    win_width/2 - button_width/2, 650,  button_width, button_height, WHITE, "sys.exit()")
 
 menu_rect_list = [baseplate, play_button, highscore_button,
-                  controls_button, main_menu_ex_cordit_button]
-# All the buttons in the control window.
+                  help_button, main_menu_ex_cordit_button]
+# All the button and rectangles in the control window.
 control_back_button = Button(0, 0,  200, 50, WHITE, "main_menu_run()")
 
-control_rect_list = [control_back_button] # DO NOT PUT BASEPLATE IN HERE
+control_rect_list = [control_back_button] #! DO NOT PUT BASEPLATE IN HERE!!!
 
-# All the buttons in the highscore window.
+# All the button and rectangles in the highscore window.
 highscore_back_button = Button(0, 0, 200, 50, WHITE, "main_menu_run()")
 
 highscore_rect_list = [baseplate, highscore_back_button]
 
-# all the button in the gameplay window
+# All the button and rectangles in the gameplay window.
 gameplay_back_menu = Button(0, 0,  200, 70, WHITE, "main_menu_run()")
 
 gameplay_header_rect = Button(210, 0, win_width - 210, 70, WHITE, "")
@@ -97,7 +94,7 @@ gameplay_rect_list = [baseplate, gameplay_back_menu, gameplay_header_rect]
 
 
 class Character:
-    """character class for the character the user will play as"""
+    """Character class for the character the user will play as."""
 
     def __init__(self, x_cord, y_cord, width, height, colour, lives, speed, answer, score):
 
@@ -112,19 +109,19 @@ class Character:
         self.score = score
 
     def make_player(self):
-        """makes the players hitbox"""
+        """Creates and returns a rectangle used as the players hitbox."""
         hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
         return hitbox
 
 
-# the gameplay character
+# The character used by the user.
 gp_character = Character(200, 100, 50, 50,  YELLOW, 3, 13, 0, 0)
 
 gp_char_list = [gp_character]
 
 
 class Enemy:
-    """ Eneemy class to create enemies for the player to interact with"""
+    """Enemy class to create enemies for the player to interact with."""
 
     def __init__(self, x_cord, y_cord, width, height, colour, value, activated, speed):
 
@@ -138,14 +135,14 @@ class Enemy:
         self.speed = speed
 
     def make_enemy(self):
-        """makes a new enemy"""
+        """Makes and returns a rectangle hitbox used by enemies."""
         enemy_hitbox = pygame.Rect(
             self.x_cord, self.y_cord, self.width, self.height)
 
         return enemy_hitbox
 
     def make_enemy_answer(self):
-        """makes enemies text answers"""
+        """Makes the font used on the enemies answer text."""
         enemy_text = pygame.font.SysFont("ariel", 100)
         return enemy_text
 
@@ -167,14 +164,14 @@ class Bullets:
         self.speed = speed
 
     def make_bullet(self):
-        """makes a new bullet"""
+        """Makes and returns a bullets hitbox."""
         bullet_hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
         return bullet_hitbox
 
 gp_bullet_list = []
 
 class Text:
-    """makes new text boxes through the Text class"""
+    """Makes new text boxes through the Text class."""
 
     def __init__(self, x_cord, y_cord, font, font_size, colour, char):
 
@@ -186,34 +183,34 @@ class Text:
         self.char = char
 
     def make_text(self):
-        """creates the text font"""
+        """Creates and returns a fint for a text object."""
         text = pygame.font.SysFont(self.font, self.font_size)
         return text
 
 
-# all the text on the main menu
+# All the text on the main menu represented as the 'Text' class.
 title_text = Text(450, 0, "", 100, WHITE, "Math Devout, Figuring It Out")
-play_button_text = Text(win_width/2 - 60, 110, "ariel", 30, BLACK, "Play Game")
+play_button_text = Text(win_width/2 - button_width/2 + 10, 225, "ariel", 50, BLACK, "Play Game")
 highscore_button_text = Text(
-    win_width/2 - 60, 175, "ariel", 30, BLACK, "Highscores")
-controls_button_text = Text(win_width/2 - 60, 250,
-                            "ariel", 30, BLACK, "Controls")
+    win_width/2 - button_width/2 + 5, 375, "ariel", 50, BLACK, "Highscores")
+help_button_text = Text(win_width/2 - button_width/2 + 25, 525,
+                            "ariel", 50, BLACK, "Help")
 main_menu_exit_text = Text(
-    win_width/2 - 60, 325, "ariel", 30, BLACK, "Exit")
+    win_width/2 - 35, 675, "ariel", 50, BLACK, "Exit")
 
 menu_text_list = [title_text, play_button_text,
-                  highscore_button_text, controls_button_text, main_menu_exit_text]
-# All the text in the contorl window.
-control_back_text = Text(0, 0, "ariel", 50, BLACK, "Main Menu")
+                  highscore_button_text, help_button_text, main_menu_exit_text]
+# All the text in the help window represented as the 'Text' class.
+help_back_text = Text(0, 0, "ariel", 50, BLACK, "Main Menu")
 
-control_text_list = [control_back_text]
+control_text_list = [help_back_text]
 
-# All the text in the highscore window.
+# All the text in the highscore window represented as the 'Text' class.
 highscore_back_text = Text(0, 0, "ariel", 50, BLACK, "Main Menu")
 
 highscore_text_list = [highscore_back_text]
 
-# all the text on the main menu
+# All the text on the main menu represented as the 'Text' class.
 gameplay_back_menu_text = Text(5, 15, "ariel", 50, BLACK, "Main Menu")
 gameplay_lives_text = Text(1700, 0, "ariel", 75, BLACK, "Lives: 3")
 gameplay_question_text = Text(400, 0, "ariel", 100, BLACK, "Question: ")
@@ -228,30 +225,34 @@ def draw(rect_list, text_list, char_list, enemy_list, bullet_list):
     for rect in rect_list:
         pygame.draw.rect(window, rect.colour, rect.make_button())
 
-    # displaying all characters
+    # Drawing all bullets.
+    # This is done before charcters and bullets so they appear under them.
+    for bullet in bullet_list:
+        pygame.draw.rect(window, bullet.colour, bullet.make_bullet())
+
+    # Displaying all characters.
     for char in char_list:
         window.blit(spaceship, (char.x_cord, char.y_cord))
 
-    # drawing all enemies
+    # Drawing all enemies.
     for enemy in enemy_list:
         pygame.draw.rect(window, enemy.colour, enemy.make_enemy())
         enemy_text = enemy.make_enemy_answer()
         button_text = enemy_text.render(str(enemy.value), True, WHITE)
         window.blit(button_text, (enemy.x_cord, enemy.y_cord))
 
-    # drawing all enemies
-    for bullet in bullet_list:
-        pygame.draw.rect(window, bullet.colour, bullet.make_bullet())
-
-    # rendering all text items
+    # Rendering all text items.
     for text in text_list:
         button_text = text.make_text().render(
             text.char, True, text.colour)
         window.blit(button_text, (text.x_cord, text.y_cord))
+
+    # refreshing the pygame window to show the new drawings
     pygame.display.update()
 
 def main_menu_run():
     """Updates and runs the main menu"""
+    # Timer making the game run 60 times each second.
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -275,24 +276,25 @@ def main_menu_run():
 
         draw(menu_rect_list, menu_text_list, [], [], [])
 
-def control_main():
-    """helps the user learn the game"""
+def help_main():
+    """Helps the user learn the game."""
     draw([baseplate], [], [], [], [])
 
     window.blit(control_image, (250, 0))
     pygame.display.update()
 
+    # Timer making the game run 60 times each second.
     clock = pygame.time.Clock()
     run = True
     while run:
         clock.tick(FPS)
 
-        # Allowing the program to stop more cleanly
+        # Allowing the program to stop more cleanly.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-            # checking for mouse clicks to activate buttons
+            # Checking for mouse clicks to activate buttons.
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x_cord, y_cord = pygame.mouse.get_pos()
                 for rect in control_rect_list:
@@ -341,7 +343,7 @@ def highscore_main():
 
 def gameplay_main():
     """the handling center the gameplay features"""
-    enemy_speed = 7
+    enemy_speed = 8
     time = ((18.9558 * (0.753947)**enemy_speed + 2.375) - 3) * FPS
     cooldown = 0
     bullet_reload = 0
@@ -360,7 +362,7 @@ def gameplay_main():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-            # checking where the mouse has clicked and if a button is present
+            # Checking where the mouse has clicked and if a button is present.
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x_cord, y_cord = pygame.mouse.get_pos()
                 for rect in gameplay_rect_list:
@@ -370,13 +372,13 @@ def gameplay_main():
                             eval(rect.func)
                         except SyntaxError:
                             pass
-            
+
             # Creating a Bullet when the spacebar is pressed.
             if keys_pressed[pygame.K_SPACE] and bullet_reload <= 0:
                 bullet_reload = 0.7 * FPS
                 for char in gp_char_list:
-                    friend_bullet = Bullets(
-                        char.x_cord + char.width, char.y_cord + char.height/2 - 10, 40, 20, BLUE, 1, 20)
+                    friend_bullet = Bullets(char.x_cord + char.width - 25, \
+                                            char.y_cord + char.height/2, 30, 10, BLUE, 1, 20)
                     gp_bullet_list.append(friend_bullet)
 
             # Making player lose a life when nessercary.
@@ -390,7 +392,7 @@ def gameplay_main():
 
                 cooldown = FPS
 
-        # Creating 5 enemies every 3 seconds.
+        # Creates a new set of enemies when the enemies disapear of screen.
         if time >= (18.9558 * (0.753947)**enemy_speed + 2.375) * FPS and spawn_enemies:
             time = 0
             create_enemies(enemy_speed)
@@ -403,15 +405,18 @@ def gameplay_main():
         cooldown -= 1
         bullet_reload -= 1
 
-        
+
 
 def run_highscore_input():
-    """Records the users highscore name"""
+    """Records the users highscore name."""
     text = ""
+
+    # Creating new nessercary rectangles and text objects to make the input box work.
     loser_text = Text(win_width/2 - 300, win_height/2 - 200, "", 100, WHITE,
-                    "You Lost your score is: " + str(gp_character.score))
+                    "You Lost, your score is: " + str(gp_character.score))
     gameplay_text_list.append(loser_text)
-    input_help_text = Text(win_width/2 - 250, win_height/2 - 125, "", 60, WHITE, "Enter your name here")
+    input_help_text = Text(
+        win_width/2 - 250, win_height/2 - 125, "", 60, WHITE, "Enter your name here")
     gameplay_text_list.append(input_help_text)
     input_box = Button(win_width/2 - 250, win_height/2 - 75, 500, 150, WHITE, "")
     gameplay_rect_list.append(input_box)
@@ -424,14 +429,14 @@ def run_highscore_input():
     while run:
         clock.tick(FPS)
 
-        # Allowing the program to stop more cleanly
+        # Allowing the program to stop more cleanly.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-            
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
-                    # Adding the players score to the highscore database.
+                    # Finding the numerical value of each score item and sorting them from highest to lowest.
                     highscore_info = str(input_text.char + " " + str(gp_character.score))
                     highscore_list.append(highscore_info)
                     score_dict = {
@@ -444,11 +449,12 @@ def run_highscore_input():
                                 score = score + char
 
                         score_dict[item] = int(score)
-                    
+
                     sorted_score_dict = sorted(score_dict.items(), key=lambda x:x[1], reverse=True)
 
                     highscore_list.clear()
 
+                    # Adding the players score to the highscore database.
                     for item in sorted_score_dict:
                         highscore_list.append(item[0])
 
@@ -466,7 +472,7 @@ def run_highscore_input():
                 else:
                     text += event.unicode
                     input_text.char = text
-                
+
 
         draw(gameplay_rect_list, gameplay_text_list, gp_char_list, [], [])
 
@@ -557,13 +563,13 @@ def collision_decttion(char_list, enemy_list, bullet_list):
                         and bullet.y_cord < (enemy.y_cord + enemy.height) \
                         or (bullet.y_cord + bullet.height) > enemy.y_cord \
                         and (bullet.y_cord + bullet.height) < (enemy.y_cord + enemy.height):
-                        
+
                         if enemy.value == gp_character.answer:
                             enemy.x_cord = 0 - enemy.width
                             gameplay_question_text.char = "Correct!"
                             gp_character.score += 1
                             gameplay_score_text.char = "Score: " + str(gp_character.score)
-                        
+
                         else:
                             pygame.event.post(pygame.event.Event(PLAYER_HIT))
                             enemy.x_cord = 0 - enemy.width
